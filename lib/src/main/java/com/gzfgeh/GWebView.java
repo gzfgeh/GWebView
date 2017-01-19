c63b1d6edbf849b4de0f9585060b1646682da14b
guzhenfu <guzhenfu@shcem.com>
2017-01-19 14:30:11 +0800

    set no cache

--- lib/src/main/java/com/gzfgeh/GWebView.java
+++ lib/src/main/java/com/gzfgeh/GWebView.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -376,7 +377,7 @@ public class GWebView extends FrameLayout implements android.support.v4.widget.S
             webSettings.setGeolocationDatabasePath(dir);
             webSettings.setGeolocationEnabled(true);// 启用地理定位
             webSettings.setDomStorageEnabled(true);
-
+            webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
             webSettings.setDefaultFontSize((int)15);
         }
 
